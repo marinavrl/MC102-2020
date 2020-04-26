23a1a9866750fdaa4c39e7fb1ce11311a440798e
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-04-26 13:25:13 -0300

    tarefa05correcao

--- tarefa05/triangulo.py
+++ tarefa05/triangulo.py
@@ -7,11 +7,11 @@ b = float(b)
 c = float(c)
 
 # termine esse programa aqui...
-if c**2 + b**2 == a**2:
+if c**2 + b**2 == a**2 and a != 0 and b != 0 and c != 0:
     print("retângulo")
-elif c**2 + b**2 < a**2 and (c**2 + b**2 - a**2)/2*b*c > -1 and (c**2 + b**2 - a**2)/2*b*c < 0:
+elif c**2 + b**2 < a**2 and (c**2 + b**2 - a**2)/2*b*c > -1 and (c**2 + b**2 - a**2)/2*b*c < 0 and a != 0 and b != 0 and c != 0:
     print("obtusângulo")
-elif c**2 + b**2 > a**2 and (c**2 + b**2 - a**2)/2*b*c > 0 and (c**2 + b**2 - a**2)/2*b*c < 1 or c == b == a:
+elif c**2 + b**2 > a**2 and (c**2 + b**2 - a**2)/2*b*c > 0 and (c**2 + b**2 - a**2)/2*b*c < 1 or c == b == a != 0:
     print("acutângulo")
 else:
     print("não forma triângulo")
