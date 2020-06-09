b542cc05cbe9fdb0458b80512c0e2979119dd9ca
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-06-09 18:46:20 -0300

    testecodificar.py correcao da ordem da codificacao

--- tarefa10/modulo.py
+++ tarefa10/modulo.py
@@ -1,5 +1,5 @@
 def codificar(imagem):
-        """le uma matriz de 0 e 1 e codifica para run-length"""
+"""le uma matriz de 0 e 1 e codifica para run-length"""
     codificacao = []
     m = len(imagem)
     n = len(imagem[0])
@@ -12,7 +12,7 @@ def codificar(imagem):
             continue
         else:
             for j in range(n):
-                if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
+                if imagem[i][j] == '0' and imagem[i + 1][j] == '0': # ver se mudando tudo ra if faz sentido
                     a += 1
                 elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
                     b += 1
@@ -20,22 +20,32 @@ def codificar(imagem):
                     c += 1
                 elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
                     d += 1
-    if a != 0:
-        codificacao.append(str(a))
-        codificacao.append('00')
-    if b != 0:
-        codificacao.append(str(b))
-        codificacao.append('01')
-    if c != 0:
-        codificacao.append(str(c))
-        codificacao.append('10')
-    if d != 0:
-        codificacao.append(str(d))
-        codificacao.append('11')
+    for i in range(m):
+        if i % 2 != 0:
+            continue
+        else:
+            for j in range(n):
+                if imagem[i][j] == '0' and imagem[i + 1][j] == '0' and a != 0:
+                    if str(a) and '00' not in codificacao:
+                        codificacao.append(str(a))
+                        codificacao.append('00')
+                elif imagem[i][j] == '0' and imagem[i + 1][j] == '1' and b != 0:
+                    if str(b) and '01' not in codificacao:
+                        codificacao.append(str(b))
+                        codificacao.append('01')
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '0' and c != 0:
+                    if str(c) and '10' not in codificacao:
+                        codificacao.append(str(c))
+                        codificacao.append('10')
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '1' and d != 0:
+                    if str(d) and '11' not in codificacao:
+                        codificacao.append(str(d))
+                        codificacao.append('11')
     return codificacao
 
 
 def decodificar(largura, altura, codificacao):
+
     return imagem
 
 
---
+++ tarefa10/testedecodificar.py
@@ -0,0 +1,6 @@
+def decodificar(largura, altura, codificacao):
+    #escrever o algoritmo
+    
+    return imagem
+
+codificacao = ['4', '00', '4', '01', '16', '11'] #exemplo de codificacao resultante da funcao codificar
