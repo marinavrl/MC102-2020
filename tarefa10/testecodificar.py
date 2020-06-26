930724605f6b70fb88bf288ee4250d7f81c35876
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-06-25 22:36:28 -0300

    codificar reduzida

--- tarefa10/testecodificar.py
+++ tarefa10/testecodificar.py
@@ -4,78 +4,75 @@ def codificar(largura, altura,imagem):
     b = 0
     c = 0
     d = 0
-    for i in range(len(imagem)):
-        if i % 2 != 0:
-            continue
-        else:
-            for j in range(len(imagem[0])):
-                if j != len(imagem[0])-1 and i != len(imagem) - 1:
-                        if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
-                            a += 1
-                            if imagem[i][j + 1] != '0' and imagem[i + 1][j + 1] != '0':
-                                codificacao.append(str(a))
-                                codificacao.append('00')
-                                a = 0
-                        elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
-                            b += 1
-                            if imagem[i][j + 1] != '0' or imagem[i + 1][j + 1] != '1':
-                                codificacao.append(str(b))
-                                codificacao.append('01')
-                                b = 0
-                        elif imagem[i][j] == '1' and imagem[i + 1][j] == '0':
-                            c += 1
-                            if imagem[i][j + 1] != '1' or imagem[i + 1][j + 1] != '0':
-                                codificacao.append(str(c))
-                                codificacao.append('10')
-                                c = 0
-                        elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
-                            d += 1
-                            if imagem[i][j + 1] != '1' or imagem[i + 1][j + 1] != '1':
-                                codificacao.append(str(d))
-                                codificacao.append('11')
-                                d = 0
-                elif j == len(imagem[0])-1 and i < len(imagem) - 2:
+    for i in range(0, len(imagem), 2):
+        for j in range(len(imagem[0])):
+            if j != len(imagem[0])-1 and i != len(imagem) - 1:
                     if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
                         a += 1
-                        if imagem[i + 2][0] != '0' or imagem[i + 3][0] != '0':
+                        if imagem[i][j + 1] != '0' and imagem[i + 1][j + 1] != '0':
                             codificacao.append(str(a))
                             codificacao.append('00')
                             a = 0
                     elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
                         b += 1
-                        if imagem[i + 2][0] != '0' and imagem[i + 3][0] != '1':
+                        if imagem[i][j + 1] != '0' or imagem[i + 1][j + 1] != '1':
                             codificacao.append(str(b))
                             codificacao.append('01')
                             b = 0
                     elif imagem[i][j] == '1' and imagem[i + 1][j] == '0':
                         c += 1
-                        if imagem[i + 2][0] != '1' and imagem[i + 3][0] != '0':
+                        if imagem[i][j + 1] != '1' or imagem[i + 1][j + 1] != '0':
                             codificacao.append(str(c))
                             codificacao.append('10')
                             c = 0
                     elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
                         d += 1
-                        if imagem[i + 2][0] != '1' and imagem[i + 3][0] != '1':
+                        if imagem[i][j + 1] != '1' or imagem[i + 1][j + 1] != '1':
                             codificacao.append(str(d))
                             codificacao.append('11')
                             d = 0
-                else:
-                    if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
-                        a += 1
+            elif j == len(imagem[0])-1 and i < len(imagem) - 2:
+                if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
+                    a += 1
+                    if imagem[i + 2][0] != '0' or imagem[i + 3][0] != '0':
                         codificacao.append(str(a))
                         codificacao.append('00')
-                    elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
-                        b += 1
+                        a = 0
+                elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
+                    b += 1
+                    if imagem[i + 2][0] != '0' and imagem[i + 3][0] != '1':
                         codificacao.append(str(b))
                         codificacao.append('01')
-                    elif imagem[i][j] == '1' and imagem[i + 1][j] == '0':
-                        c += 1
+                        b = 0
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '0':
+                    c += 1
+                    if imagem[i + 2][0] != '1' and imagem[i + 3][0] != '0':
                         codificacao.append(str(c))
                         codificacao.append('10')
-                    elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
-                        d += 1
+                        c = 0
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
+                    d += 1
+                    if imagem[i + 2][0] != '1' and imagem[i + 3][0] != '1':
                         codificacao.append(str(d))
                         codificacao.append('11')
+                        d = 0
+            else:
+                if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
+                    a += 1
+                    codificacao.append(str(a))
+                    codificacao.append('00')
+                elif imagem[i][j] == '0' and imagem[i + 1][j] == '1':
+                    b += 1
+                    codificacao.append(str(b))
+                    codificacao.append('01')
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '0':
+                    c += 1
+                    codificacao.append(str(c))
+                    codificacao.append('10')
+                elif imagem[i][j] == '1' and imagem[i + 1][j] == '1':
+                    d += 1
+                    codificacao.append(str(d))
+                    codificacao.append('11')
     return codificacao
     """"if j != len(imagem[0])-1 and i != len(imagem) - 1:
                     if imagem[i][j] == '0' and imagem[i + 1][j] == '0':
