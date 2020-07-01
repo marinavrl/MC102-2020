3ce7b2a5afd0bf9bbb360d3ddcf19047018af8f3
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-07-01 15:28:58 -0300

    codificacao o que eu faria apos ajuda pra reduzir

--- tarefa10/modulo.py
+++ tarefa10/modulo.py
@@ -5,16 +5,16 @@ def codificar(largura,altura,imagem):
     a = 1 #define antes
     for i in range(0,altura,2):
         for j in range(largura):
-            point = True #ajuda a marcar que nao ta repetindo contagem
+            point = 0 #ajuda a marcar que nao ta repetindo contagem
             if j < largura - 1:#faz a soma de strings
                 if imagem[i][j] + imagem[i+1][j] == imagem[i][j+1] + imagem[i+1][j+1]:
                     a+=1
-                    point = False
+                    point = 1
             if j == largura - 1 and i+2<altura:
                 if imagem[i][j] + imagem[i+1][j] == imagem[i+2][0] + imagem[i+3][0]:
                     a+=1
-                    point = False
-            if point or i+j==(altura + largura - 3):
+                    point = 1
+            if point == 0 or i+j==(altura + largura - 3):
                 codificacao.append(str(a))
                 codificacao.append(imagem[i][j] + imagem[i+1][j])
                 a = 1   #comeca a contar de novo
