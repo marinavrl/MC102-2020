78b79538f5fb23d37a58a37f86a20513f25091fa
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-07-02 22:16:12 -0300

    rascunho stubs

--- tarefa11/frequencia.py
+++ tarefa11/frequencia.py
@@ -1,4 +1,4 @@
-"""tarefa: dicionarios e tuplas"""
+"""tarefa: dicionarios e tuplas""" #ler a parte de conjuntos unidade de colecoes
 """Stop words: artigos, preposicoes e conectivos.
 O programa: conta a freq com que as palavras aparecem 
 nos arquivos de texto
@@ -18,26 +18,33 @@ desconsidere palavras que se repetem 5 vezes ou menos.
 """
 #quartil== pega as que tem freq>=5 e organiza em ordem decrescente depois pega o primeiro quarto dessas,
 # o quartil vai ser a ultima palavra desse quarto
-texto = input()
-with open(texto) as arquivo:
-    L = []
-    for line in arquivo:
-        Ls = line.strip().split()
-        L.append(Ls)
-for a in L:
-    if a == []:
-        L.remove(a)
-L0 = []
-for i in range(len(L)):
-    for b in L[i]:
-        L0.append(b)
-conjunto = set(L0)
-print(conjunto) """conjunto com todas as palavras do arquivo, falta procurar as certas pra cada caso """
-#conjunto = set(L)
-#print(L)
-#nome2 = input()
-#nome3 = input()
-#print(f'Hello, {nome1}.') FAZER LISTA/DICIONARIO PARA SEPARAR AS 3 MAIS FREQ?
-#print(f'Hello, {nome2}.') SEGUNDA LINHA
-#print(f'Hello, {nome3}.') TERCEIRA LINHA
+def listar_palavras_arquivo(texto):
+    with open(texto) as arquivo:
+        L = []
+        for line in arquivo:
+            Ls = line.strip().split()
+            L.append(Ls)
+    for a in L:
+        if a == []:
+            L.remove(a)
+    L0 = []
+    for i in range(len(L)):
+        for b in L[i]:
+            L0.append(b)
+    return L0 #melhor manter a lista pra contar a freq
+#conjunto = set(L0)
+#print(conjunto)
+def tirar_stopwords(L0, stopwords): # recebe lista de stopwords da entrada, ver entrada do problema
+    """ identifica as stop words em L0 e remove"""
+    pass
+def contar_frequencia():
+    """ Recebe a lista sem stopwords e ordena da mais para a menos frequente, retornando uma lista organizada"""
+    pass
 
+def main():
+    texto = "testes/texto0.in" #input
+    stopwords = [] #lista recebe as stopwords
+    L0 = listar_palavras_arquivo(texto)
+
+"""conjunto com todas as palavras do arquivo, falta procurar as certas pra cada caso """
+#conjunto = set(L)
