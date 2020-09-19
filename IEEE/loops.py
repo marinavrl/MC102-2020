c24b2bac11d4fe150d7383c73f3b513577517b71
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-09-19 19:25:57 -0300

    IEEE

---
+++ IEEE/loops.py
@@ -0,0 +1,28 @@
+"""numero = 1
+while (numero <= 10) :
+    print("O número é:", numero)
+    numero += 1"""
+
+"""numero = 10
+while (numero >= 0):
+    print("O número é:", numero)
+    numero -= 1"""
+
+"""print("Meus sorvetes favoritos:")
+for sabor in ["Negresco", "Chocomenta", "Torta de Limao", "Rafaello"]:
+    print(sabor)"""
+
+#1
+for i in range(10):
+    print("#" * (i + 1))
+#2
+for i in range(1, 7):
+    print("#" * i)
+#3
+for numero in range(10, -5, -1):
+    print("numero vale", numero)
+#4
+for numero in range(2, 20, 2):
+    print(numero, "eh par")
+
+
--- tarefa11/sugestao.py
+++ tarefa11/sugestao.py
@@ -1,1 +1,3 @@
 """ usar a contagem da frequencia.py para as freq e ver qual aparece mais depois das palavras sugeridas"""
+"""Dadas duas palavras, sugere ao usuário qual será a próxima palavra. Essa escolha é baseada na probabilidade 
+de que essas três palavras apareçam nessa ordem em um determinado texto."""
