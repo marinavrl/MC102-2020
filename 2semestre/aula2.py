79fd16d8d5e547ec96aff403b7986f05bcdec6da
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-09-22 17:53:38 -0300

    lab01.py

---
+++ 2semestre/aula2.py
@@ -0,0 +1,23 @@
+print("Hello", "world!", sep = "+")
+
+print("Hello\n","world!")
+
+#deixar tudo junto
+print("Unicamp", end = "")
+2 print("MC102!")
+
+#deixar em duas linhas sem espaço
+print("Hello\n","world!", sep = "")
+
+#Com o caractere de controle de quebra de linha (\n) no fim:
+1 print("MC102", "Unicamp", "2020", sep = " - ", end = "!\n")
+2 print("Novo Texto!")
+
+
+# Parâmetros importantes da função print
+# sep: Texto usado na separação dos argumentos recebidos.
+# end: Texto impresso no final da execução da função.
+print("MC102", "Unicamp", sep = " - ", end = "!\n")
+# MC102 - Unicamp! seguido de mudança de linha
+
+
