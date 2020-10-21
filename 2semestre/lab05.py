b14ac3ae6208c3bd8f6ff4bef83356b4475cc1ac
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-10-21 15:24:42 -0300

    lab05.py rascunho

---
+++ 2semestre/lab05.py
@@ -0,0 +1,35 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 5 - Números da Mega-Sena
+# Nome: Marina Victória Roncalio de Lima
+# RA: 241482
+###################################################
+
+"""João sempre entrega no caixa da lotérica suas apostas com os seis números ordenados de maneira crescente.
+Os números devem ter paridades alternadas. Ou seja, se o primeiro número for par, o segundo deve ser ímpar,
+o terceiro par e assim por diante. Se o primeiro número for ímpar, o segundo deve ser par, o terceiro ímpar 
+e assim por diante.
+A soma dos 6 números da aposta não pode ser um número múltiplo de 7 ou 13. """
+
+"""Lista com os n2 e outra lista com os n5 de acordo com os numeros fornecidos"""
+
+"""laco para criar as possibilidades de listas a partir da lista de n2 combinada a lista de n5
+Se a lista estiver de acordo com as restricoes, devolve como saida a cada laco """
+
+# Leitura de dados
+n1 = int(input())
+n3 = int(input())
+n4 = int(input())
+n6 = int(input())
+
+# Impressão dos quatro números fornecidos como entrada
+
+print("Primeiro:", "{:02}".format(n1))
+print("Terceiro:", "{:02}".format(n3))
+print("Quarto:", "{:02}".format(n4))
+print("Sexto:", "{:02}".format(n6))
+
+# Processamento e impressão da lista de possíveis apostas
+
+print("Lista de apostas:")
+print("{:02} - {:02} - {:02} - {:02} - {:02} - {:02}".format(n1, n2, n3, n4, n5, n6))
