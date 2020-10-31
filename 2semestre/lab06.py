65cdbaf0872590418abae5e934c75295c06d1d9e
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-10-31 16:31:13 -0300

    lab06.py rascunho

--- 2semestre/lab06.py
+++ 2semestre/lab06.py
@@ -5,9 +5,33 @@
 # RA: 241482
 ###################################################
 
+""" Programa para auxiliar na compra e venda das
+ações da empresa """
+
 # Leitura de dados
 
+"""Programa recebe um valor N (número de dias que 
+você tem registro do preço das ações); N <= 30 """
 
+"""Nas próximas N linhas, serão lidos os preços
+diários das ações """
+
+"""Será lido um valor K (número máximo de dias
+permitido entre a compra e a venda da ação) """
+
+"""Ler um valor Q (quantidade de dinheiro que
+levará na viagem) """
+
+"""Comprar e vender ações em um único dia, de modo
+a ter o maior lucro possível.
+As ações são vendidas por inteiro.
+Deve-se sempre comprar a maior quantidade possível
+de ações."""
+
+"""A saída contem o dia e o valor da compra de
+cada ação,  o dia e o valor de venda de cada ação,
+a quantidade de ações compradas, e o lucro total
+obtido."""
 
 
 # Escolha da melhor variação de valores da ação
