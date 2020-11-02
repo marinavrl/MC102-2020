93a475e9e58a2fe7ada77482c88c91e4e710c2c1
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-11-02 14:55:33 -0300

    lab06 fim

--- 2semestre/lab06.py
+++ 2semestre/lab06.py
@@ -10,35 +10,6 @@ ações da empresa """
 
 # Leitura de dados
 
-"""O preço varia por dia, então, se você comprar
-num dia em que a ação está mais barata e vender
-quando estiver mais cara você obtém um lucro
-maior, lembrando do limite de tempo para
-vendê-las."""
-
-"""Programa recebe um valor N (número de dias que 
-você tem registro do preço das ações); N <= 30 """
-
-"""Nas próximas N linhas, serão lidos os preços
-diários das ações """
-
-"""Será lido um valor K (número máximo de dias
-permitido entre a compra e a venda da ação) """
-
-"""Ler um valor Q (quantidade de dinheiro que
-levará na viagem) """
-
-"""Comprar e vender ações em um único dia, de modo
-a ter o maior lucro possível.
-As ações são vendidas por inteiro.
-Deve-se sempre comprar a maior quantidade possível
-de ações."""
-
-"""A saída contem o dia e o valor da compra de
-cada ação,  o dia e o valor de venda de cada ação,
-a quantidade de ações compradas, e o lucro total
-obtido."""
-
 N = int(input())
 
 lista_valores = [] #lista para armazenar os valores diários das ações
@@ -47,8 +18,6 @@ for _ in range(N):
     valor_dia = float(input())
     lista_valores.append(valor_dia)
 
-print(lista_valores)
-
 K = int(input())
 
 Q = float(input())
@@ -56,8 +25,6 @@ Q = float(input())
 #criar lista em ordem crescente de valores
 
 lista_ordenada = sorted(lista_valores)
-
-print(lista_ordenada)
 
 # Escolha da melhor variação de valores da ação
 
@@ -70,7 +37,7 @@ for a in lista_valores:
         
 
 if valor_venda != lista_valores[0]: #escolhendo valor compra
-    lista_possiveis = lista_valores[i-K:i+1].sort()
+    lista_possiveis = sorted(lista_valores[i-K:i+1])
     valor_compra = lista_possiveis[0]
 
 else:
@@ -81,8 +48,6 @@ dia_compra = lista_valores.index(valor_compra) + 1
 qtde_acoes = int(Q // valor_compra)
 
 lucro = qtde_acoes * (valor_venda - valor_compra)
-
-"lucro = obtido - custo"
 
 # Saída de dados
 
