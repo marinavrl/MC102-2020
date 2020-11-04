e22b3665f8842aad8a2009cf909221033ba13f65
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-11-04 15:45:18 -0300

    tstlab06_variacao.py correcao 2

--- 2semestre/tstlab06_variacao.py
+++ 2semestre/tstlab06_variacao.py
@@ -8,28 +8,29 @@ K = 2
 
 lucro_maximo = 0
 
-qtde_acoes = 0
+qtde_acoes1 = 0
 
 for a in lista_compra:
     qtde_acoes0 = int(Q//a)
-    if qtde_acoes0 > qtde_acoes: #ta chateando aqui
-        qtde_acoes = qtde_acoes0
+    if qtde_acoes0 > qtde_acoes1: #continua aqui e vai até o ultimo laco
+        qtde_acoes1 = qtde_acoes0
     for b in lista_venda:
         if lista_compra.index(a) <= lista_venda.index(b) <= lista_compra.index(a) + K:
-            lucro_maximo0 = qtde_acoes * (b - a)
+            lucro_maximo0 = qtde_acoes1 * (b - a)
             if lucro_maximo0 > lucro_maximo:
                 lucro_maximo = lucro_maximo0
                 valor_compra = a
                 valor_venda = b
                 dia_compra = lista_compra.index(a) + 1
                 dia_venda = lista_venda.index(b) + 1
-                qtde_acoes = int(Q//valor_compra)
-                print(qtde_acoes)
-                lucro_maximo = qtde_acoes * (valor_venda - valor_compra)
+                qtde_acoes1 = int(Q//valor_compra)
+                print(qtde_acoes1)
+                lucro_maximo = qtde_acoes1 * (valor_venda - valor_compra)
 
 print(valor_compra, valor_venda)
 print(dia_compra, dia_venda)
 print(lucro_maximo)
+qtde_acoes = int(Q//valor_compra)
 print(qtde_acoes)
 
 
