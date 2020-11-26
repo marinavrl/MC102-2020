4b9016b5197af107ee3e101f81e551b314e04cdf
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-11-26 10:27:56 -0300

    mt fim

--- 2semestre/matriz_transposta.py
+++ 2semestre/matriz_transposta.py
@@ -16,6 +16,41 @@ def lê_matriz():
             linha.append(int(i))
         M.append(linha)
 
+#calcula as dimensoes da matriz
+
+def dimensões(M):
+    linhas = len(M)
+    colunas = len(M[0])
+    for i in range(1, linhas):
+        if len(M[i]) != colunas:
+            return ()
+    return (linhas, colunas)
+
+def transposta(M):
+    T = []
+    (linhas, colunas) = dimensões(M)
+    for j in range(colunas):
+        T.append([])
+        for i in range(linhas):
+            T[j].append(M[i][j])
+    return T
+
+def imprime_matriz(M):
+    for linha in M:
+        # converte os elementos da lista para string
+        aux = [str(i) for i in linha]
+        print(" ".join(aux))
+
+
 def main():
-    M = lê_matriz
+    M = lê_matriz()
+    (linhas, colunas) = dimensões(M)
     print(M)
+    imprime_matriz(M)
+    print(linhas)
+    print(colunas)
+    T = transposta(M)
+    print(T)
+    imprime_matriz(T)
+
+main()
