0eb9ecacd64fef3f21394cd561f0e9c8228ccd59
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-11-27 18:13:43 -0300

    lab08.py inicio

---
+++ 2semestre/lab08.py
@@ -0,0 +1,27 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 8 - Ocorrência de Palavras
+# Nome: Marina Victória Roncalio de Lima
+# RA: 241482
+###################################################
+
+"""O programa deve contabilizar a freq
+da palavra sozinha bem como dentro de outras """
+
+# Leitura de dados
+
+L = int(input()) #quantidade de linhas do texto
+
+
+
+
+# Processamento do texto
+
+
+
+
+# Saída de dados
+
+print("Palavra buscada:", palavra)
+print("Ocorrencia:", ocorrencia)
+print("Palavras similares:", similares)
