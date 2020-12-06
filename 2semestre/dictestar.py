b55c56ae992516d35a2a648802fb03e84fc72f04
Marina Victoria Roncalio de Lima <m241482@dac.unicamp.br>
2020-12-06 20:05:42 -0300

    lab09rascunho.py normais e penaltis ok

---
+++ 2semestre/dictestar.py
@@ -0,0 +1,22 @@
+dic = {'Panama': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 
+'sofridos': 0}, 'Alemanha': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 
+'marcados': 0, 'sofridos': 0}, 'Inglaterra': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 
+'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Suecia': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 
+'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Brasil': {'partidas': 0, 'vitorias': 0, 
+'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Tunisia': {'partidas': 0, 
+'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 
+'Suica': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 
+'sofridos': 0}, 'Dinamarca': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 
+'marcados': 0, 'sofridos': 0}, 'Argentina': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 
+'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Peru': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 
+'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Uruguai': {'partidas': 0, 'vitorias': 0, 
+'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Colombia': {'partidas': 0, 
+'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}, 
+'Chile': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 
+'sofridos': 0}, 'Marrocos': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 'normal': 0, 
+'marcados': 0, 'sofridos': 0}, 'Japao': {'partidas': 0, 'vitorias': 0, 'derrotas': 0, 'penaltis': 0, 
+'normal': 0, 'marcados': 0, 'sofridos': 0}, 'Franca': {'partidas': 0, 'vitorias': 0, 
+'derrotas': 0, 'penaltis': 0, 'normal': 0, 'marcados': 0, 'sofridos': 0}}
+
+selecao = input()
+print(dic[selecao]["partidas"])
--- 2semestre/lab09rascunho.py
+++ 2semestre/lab09rascunho.py
@@ -2,7 +2,7 @@
 
 selecoes = []
 dic = {}
-for i in range(2):
+for i in range(16):
   selecao = input()
   selecoes.append(selecao)
   dic[selecao] = {"partidas": 0, #dicionario de duas dimensões
@@ -23,9 +23,55 @@ print(dic)
 que ocorreram até o final da competição, também sendo um 
 resultado por linha"""
 
-"""for i in range(16):
+resultados = []
+
+for i in range(16):
   resultado = input()
   resultado_split = resultado.split()
+  resultados.append(resultado_split)
+
+print(resultados)
+
+C = {"0", "(0", "0)", "1", "(1", "1)", "2", "(2", "2)",
+ "3", "(3", "3)", "4", "(4", "4)", "5", "(5", "5)", "6", "(6", "6)", "7", "(7", "7)", 
+ "8", "(8", "8)", "9", "(9", "9)", "10", "(10", "10)", "x"}
+
+D = {"(0", "0)", "(1", "1)", "(2", "2)", "(3", "3)", "(4", "4)", "(5", "5)", "(6", "6)", "(7", "7)", 
+"(8", "8)", "(9", "9)", "(10", "10)"}
+
+for result in resultados:#contar a quantidade total de partidas etc
+  for i in range(len(result)):
+    if result[i] not in C:
+      pais = result[i]
+      dic[pais]["partidas"] += 1 #ok
+    elif result[i] in C: #partidas normais ou penaltis
+        if i == 1:
+          if result[4] in D:
+            pais = result[0]
+            dic[pais]["penaltis"] += 1
+          else:
+            pais = result[0]
+            dic[pais]["normal"] += 1
+        elif i == 3:
+          if result[4] in D:
+            pais = result[7]
+            dic[pais]["penaltis"] += 1
+          else:
+            pais = result[4]
+            dic[pais]["normal"] += 1
+
+"""elif result[i] in D: #partidas penaltis
+      if i == 4:
+        pais = result[0]
+        dic[pais]["penaltis"] += 1
+      elif i == 6:
+        pais = result[7]
+        dic[pais]["penaltis"] += 1"""
+
+
+print(dic)
+
+
 #for pais in resultado_split:"""
 #comparar com a chave correspondente no dic e somar
 #nos zeros
